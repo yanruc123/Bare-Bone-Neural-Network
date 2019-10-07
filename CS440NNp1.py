e127e400bdebc98c019b7b8c4be5a997651d87fa
Yanru Chen <47041483+yanrucbu@users.noreply.github.com>
2019-10-06 20:35:16 -0400

    c2

--- CS440NNp1.py
+++ CS440NNp1.py
@@ -25,22 +25,24 @@ class NeuralNetwork:
         """
         # Initialize your weight matrices first.
         # (hint: check the sizes of your weight matrices first!)
-        W1 = np.random()
-        W2
+        W1 = np.random.rand(input_nodes,NNodes)
+        W2 = np.random.rand(NNodes,output_nodes)
+
+        #record 2 list of testing result
         forward_output = []
         back_output = []
         
         # For each epoch, do
-        for e in epoch:
+        for e in epochs:
             # For each training sample (X[i], Y[i]), do
             for i in range(0,len(X)):
                 sample = (X[i],Y[i])
                 # 1. Forward propagate once. Use the function "forward" here!
-                forward_output.append(forward(sample))
+                forward_output.append(forward(sample,W1,W2))
                 
                 # 2. Backward progate once. Use the function "backpropagate" here!
-                back_output.append(backpropagate(sample))
-                
+                back_output.append(backpropagate(sample,W1,W2))
+
         pass
         
 
@@ -58,29 +60,34 @@ class NeuralNetwork:
         """
         return YPredict
 
-    def forward(self, X):
+    def forward(self, X, W1, W2):
         # Perform matrix multiplication and activation twice (one for each layer).
         # (hint: add a bias term before multiplication)
 
         # X = matrix of input nodes
         # W1 = matrix of weight between input and first hidden layer
         # W2 = matrix of weight between second hidden layer and output layer
-        self.hiddenLayer1 = sigmoid(np.dot(X,W1))
-        self.hiddenLayer2 = np.dot(self.hiddenTrans1,W2)
-        self.output = sigmoid(self.hiddenLayer2)
+        self.z2 = np.dot(X,W1)
+        self.a2 = sigmoid(self.z2)
+        self.z3 = np.dot(a2,W2)
+        yhat = sigmoid(self.z3)
 
-        return output
+        return yhat
 
     def sigmoid(self, z):
         return 1/(1+np.exp(-z))
+
+    def sigmoidDeriv(self,z):
+        return np.exp(-z)/((1+np.exp(-z))**2)
         
-    def backpropagate(self):
+    def backpropagate(self,X,W1,W2):
         # Compute loss / cost using the getCost function.
-                
-                
+        costderiv3 = 
+        costderiv2 =       
                 
         # Compute gradient for each layer.
-        
+        djdw2 = np.dot(np.transpose(self.a2),costderiv3)
+        djdw1 = np.dot(np.transpose(X),costderiv2)
         
         
         # Update weight matrices.
@@ -89,6 +96,12 @@ class NeuralNetwork:
     def getCost(self, YTrue, YPredict):
         # Compute loss / cost in terms of crossentropy.
         # (hint: your regularization term should appear here)
+
+        # for each term in (ypredict - ytrue)^2 *0.5
+        # TODO: find out dimension of ytrue and y predict and 修改
+        -(YPredict-YTrue)*(map(sigmoidDeriv,self.z3)
+        result = 0.5*(YPredict - YTrue)**2
+        return result
         pass
 
 def getData(dataDir):
